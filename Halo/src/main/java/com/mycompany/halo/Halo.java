3a2643d083032f915a6f12f59f5bc4c0c422d65e
Kyky-Dev <pungky.prameswari@gmail.com>
2025-01-02 22:35:07 +0700

    Add files via upload

---
+++ Halo/src/main/java/com/mycompany/halo/Halo.java
@@ -0,0 +1,9 @@
+package com.mycompany.halo;
+
+public class Halo {
+    public static void main(String[] args) {
+        System.out.println("Halo Indonesia");
+        System.out.println("Selamat Pagi Indonesia");
+        System.out.println("Jaya Indonesia");
+    }
+}
